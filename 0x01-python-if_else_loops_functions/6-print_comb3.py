12246f3e809fa22973c4bd74db598a545de48efa
Adzi-Tay Emmanuel <adzitayemmanuel@gmail.com>
2023-03-09 13:51:41 -0800

    Python is fun

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if (not((j== i) or (i > j))):
-            print("{}".format(i), end="")
-            print("{}".format(j), end="");
+            print("{}{}".format(i, j), end="");
             if (not(j == 9 and i == 8)):
                 print("{}".format(', '), end="")
+print()
