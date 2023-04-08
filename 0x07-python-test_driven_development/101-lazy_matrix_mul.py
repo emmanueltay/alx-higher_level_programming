837edec6e2d153b5149987c080e1c1a43f37393d
Adzi-Tay Emmanuel <adzitayemmanuel@gmail.com>
2023-04-07 18:48:48 -0700

    Done

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-""" use numpy to multiply two matrices the lazy way"""
+"""A module that contains the function 'lazy_matrix_mul'"""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-        """ Function that takes in 2 matrices and multiplies them
-        Args:
-            m_a: first matrix
-            m_b: second matrix
-        Return: the product of m_a and m_b
-        """
-        return np.matmul(m_a, m_b)
+    """returns the product of two conformable matrices in a lazy way"""
+    return np.matmul(m_a, m_b)
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,35 +1,34 @@
 #!/usr/bin/python3
-""" module that divides all elements of a matrix of similar sized rows """
+"""Defines a matrix division function"""
 
 
 def matrix_divided(matrix, div):
-    """ function that returns a new matrix with each element divided by da div
+    """Divide all elements of a matrix
     Args:
-        matrix: a 2d array, each row should be the same size or else: error
-        div: a number that is not 0 or else error
-    Returns: a new matrix with each element adjusted to the div amount
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor.
+    Raises:
+        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float
+        ZeroDivisionError: If div is 0
+    Returns:
+        A new matrix representing the result of the division.
     """
-    message = "matrix must be a matrix (list of lists) of integers/floats"
-    new = []
-    if not matrix or matrix is [[]] or matrix is None:
-        raise TypeError(message)
-    if type(div) is int or type(div) is float or div is None:
-        pass  # improper div value type checks
-    else:
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((type(element) in [int, float]) for element in
+                    [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-    if div == 0:  # if div is zero
+
+    if div == 0:
         raise ZeroDivisionError("division by zero")
-    if matrix[0]:  # if matrix is empty or not
-        length = len(matrix[0])
-    else:
-        raise TypeError(message)
-    for i in range(len(matrix)):  # real work. the appending
-        if len(matrix[i]) is not length:
-            raise TypeError("Each row of the matrix must have the same size")
-        new.append([])
-        for j in matrix[i]:
-            if type(j) is int or type(j) is float:
-                new[i].append(round(j / div, 2))
-            else:
-                raise TypeError(message)
-    return 
+
+    return [[round(num / div, 2) for num in row] for row in matrix] 
