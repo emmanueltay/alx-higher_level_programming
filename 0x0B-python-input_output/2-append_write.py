6778662c20adeeccd87b5d8a116400b5ef396350
Adzi-Tay Emmanuel <adzitayemmanuel@gmail.com>
2023-04-12 17:25:44 -0700

    Input/Output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Contains the function append_write(...)"""
+
+
+def append_write(filename="", text=""):
+    """appends a string at the end of a text file (UTF8)
+    
+    Returns: the number of characters added"""
+    with open(filename, "a", encoding="utf-8") as f:
+        count = f.write(text)
+    return count
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+This project is on File input/output in Python
